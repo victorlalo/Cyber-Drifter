8cd670e9bac5ac21bd84c974180882ebb1de172c
Victor Lalo <vlalo0624@gmail.com>
2020-08-25 20:22:14 -0700

    Added Start Screen and began work on pause menu

---
+++ Assets/Scripts/Effects/Menus/TitleFX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class TitleFX : MonoBehaviour
+{
+    [SerializeField] Text titleText;
+    [SerializeField] Color startColor;
+    [SerializeField] Color endColor;
+
+    [SerializeField] float transitionDuration = 1f;
+
+    void Start()
+    {
+        titleText.color = startColor;
+        titleText.DOColor(endColor, transitionDuration).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutQuad);
+    }
+
+    void Update()
+    {
+        
+    }
+}
