bd0abe4c13c4a10daeabab3471113562e4e2d0a1
Victor Lalo <vlalo0624@gmail.com>
2020-09-06 23:46:17 -0700

    Progress on pickup items and ramps

---
+++ Assets/Scripts/Pickups/FloatingAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FloatingAnimation : MonoBehaviour
+{
+    float delay;
+
+    void Start()
+    {
+        delay = Mathf.Sin(transform.position.z);
+        StartAnimation();
+    }
+
+    void StartAnimation()
+    {
+        transform.DOMoveY(1, .5f).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo).SetDelay(delay);
+        transform.DORotate(new Vector3(0, 360, 0), .85f, RotateMode.WorldAxisAdd).SetLoops(-1).SetDelay(delay);
+    }
+
+
+}
---
+++ Assets/Scripts/Pickups/PickupItem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public enum ItemType
+{
+    TOKEN,
+    SHIELD,
+    MUILTIPLIER,
+    DIVIDER,
+    NITROUS
+}
+
+public class PickupItem : MonoBehaviour
+{
+    [SerializeField] ItemType itemType;
+
+    public event Action<ItemType> onItemCollision;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("GOT ITEM: " + itemType.ToString());
+            onItemCollision?.Invoke(itemType);
+            Destroy(gameObject);
+        }
+            
+    }
+}
